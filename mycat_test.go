ba3f0622f6de42de9a6317e5d676d09957b9114c
utam0k <k0ma@utam0k.jp>
2021-09-10 23:45:22 +0900

    up.

--- mycat_test.go
+++ mycat_test.go
@@ -6,9 +6,13 @@ import (
 	"testing"
 )
 
-func TestMycat(t *testing.T) {
+func TestCountNumberOfLines(t *testing.T) {
 	tmpFile, _ := ioutil.TempFile("", "tmptest")
 	defer os.Remove(tmpFile.Name())
+	expectedNumberOfLines := 10
+	for i := 0; i < expectedNumberOfLines; i++ {
+		tmpFile.WriteString("\n")
+	}
 
 	type args struct {
 		file string
@@ -20,14 +24,14 @@ func TestMycat(t *testing.T) {
 	}{
 		{
 			name:   "happy path",
-			args:   args{file: "Makefile"},
-			expect: 3,
+			args:   args{file: tmpFile.Name()},
+			expect: expectedNumberOfLines,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got, _ := count_number_of_lines(tt.args.file); got != tt.expect {
-				t.Errorf("add() = %v, want %v", got, tt.expect)
+				t.Errorf("count_number_of_lines() = %v, want %v", got, tt.expect)
 			}
 		})
 	}
