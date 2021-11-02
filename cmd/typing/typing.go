9502b5f5eb734b5aa59c761df03338fd3487d71d
utam0k <k0ma@utam0k.jp>
2021-11-02 21:45:55 +0900

    up

--- cmd/typing/typing.go
+++ cmd/typing/typing.go
@@ -3,33 +3,29 @@ package main
 import (
 	"fmt"
 	"log"
-	"sync"
 	"time"
 )
 
 func main() {
-	wg := sync.WaitGroup{}
 	ch := make(chan struct{})
 	timer := time.NewTimer(2 * time.Second)
-	wg.Add(1)
 	go func() {
-		defer wg.Done()
-		for {
-			select {
-			case <-timer.C:
-				fmt.Println("A Timer finished")
-				wg.Done()
-			case _, ok := <-ch:
-				if !ok {
-					return
-				}
-				log.Println("OK")
-			}
+		defer close(ch)
+		for i := 0; i < 10; i++ {
+			time.Sleep(time.Second * 1)
+			ch <- struct{}{}
 		}
 	}()
-	for i := 0; i < 10; i++ {
-		ch <- struct{}{}
+	for {
+		select {
+		case <-timer.C:
+			fmt.Println("A Timer finished")
+			return
+		case _, ok := <-ch:
+			if !ok {
+				return
+			}
+			log.Println("OK")
+		}
 	}
-	close(ch)
-	wg.Wait()
 }
