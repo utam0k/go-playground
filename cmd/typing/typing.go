6ebd79a0828402e96fa55dda32442afd559bc899
utam0k <k0ma@utam0k.jp>
2021-11-08 21:47:32 +0900

    up.

--- cmd/typing/typing.go
+++ cmd/typing/typing.go
@@ -2,38 +2,50 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"time"
 )
 
+func readLineLoop(ch chan<- string) {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		line, _, err := reader.ReadLine()
+		if err != nil {
+			log.Fatal("failed to read a rune.")
+		}
+		ch <- string(line)
+	}
+
+}
+
 func main() {
+	var timeoutSec = flag.Int("t", 10, "Timeout")
+	flag.Parse()
+
 	problems := [...]string{"Golang", "Rust", "Python", "Ruby", "Java", "Scala"}
 	ch := make(chan string)
-	timer := time.NewTimer(5 * time.Second)
-	go func() {
-		for {
-			reader := bufio.NewReader(os.Stdin)
-			line, _, err := reader.ReadLine()
-			if err != nil {
-				log.Fatal("failed to read a rune.")
-			}
-			ch <- string(line)
-		}
-	}()
+	timer := time.NewTimer(time.Duration(*timeoutSec) * time.Second)
+	go readLineLoop(ch)
 
-	for _, p := range problems {
-		fmt.Println(p)
+	var score int
+	for {
+		p := problems[rand.Intn(len(problems))]
+		fmt.Println("problem: " + p)
 		select {
 		case <-timer.C:
-			fmt.Println("A Timer finished")
+			fmt.Println("TIME UP")
+			fmt.Printf("score: %d\n", score)
 			return
 		case answer, ok := <-ch:
 			if !ok {
 				return
 			}
 			if p == answer {
+				score += len(p)
 				fmt.Println("OK")
 			} else {
 				fmt.Println("NG")
